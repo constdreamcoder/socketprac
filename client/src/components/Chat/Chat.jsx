64dda36b976ddd4134cdee7c6b318eba57b45703
suchan <scdreamricher@gmail.com>
2022-07-12 22:16:19 +0900

    완성!

--- client/src/components/Chat/Chat.jsx
+++ client/src/components/Chat/Chat.jsx
@@ -6,6 +6,7 @@ import "./Chat.css";
 import InfoBar from "../InfoBar/InfoBar";
 import Input from "../Input/Input";
 import Messages from "../Messages/Messages";
+import TextContainer from "../TextContainer/TextContainer";
 
 let socket;
 
@@ -13,6 +14,7 @@ const Chat = () => {
 	const location = useLocation();
 	const [name, setName] = useState("");
 	const [room, setRoom] = useState("");
+	const [users, setUsers] = useState("");
 	const [message, setMessage] = useState([]);
 	const [messages, setMessages] = useState([]);
 
@@ -39,6 +41,10 @@ const Chat = () => {
 		socket.on("message", (message) => {
 			setMessages([...messages, message]);
 		});
+
+		socket.on("roomData", ({ users }) => {
+			setUsers(users);
+		});
 	}, [messages]);
 
 	const sendMessage = (event) => {
@@ -61,6 +67,7 @@ const Chat = () => {
 					sendMessage={sendMessage}
 				/>
 			</div>
+			<TextContainer users={users} />
 		</div>
 	);
 };
--- server/index.js
+++ server/index.js
@@ -40,6 +40,11 @@ io.on("connection", (socket) => {
 
 		socket.join(user.room);
 
+		io.to(user.room).emit("roomData", {
+			room: user.room,
+			users: getUsersInRoom(user.room),
+		});
+
 		callback();
 	});
 
@@ -47,6 +52,10 @@ io.on("connection", (socket) => {
 		const user = getUser(socket.id);
 
 		io.to(user.room).emit("message", { user: user.name, text: message });
+		io.to(user.room).emit("roomData", {
+			room: user.room,
+			users: getUsersInRoom(user.room),
+		});
 
 		callback();
 	});
